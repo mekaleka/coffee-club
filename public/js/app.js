ff0fbd0396fbfae829cac1b8e554ba19691aebcf
mekaleka <www.no-reply.com>
2020-03-21 02:43:26 -0700

    finished adding ajax calls to app.js, styled a coffee appropriate background color

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,41 @@
 $(function() {
-      // Send the PUT request.
-      $.ajax("/api/coffeehouse/" {
-        type: "GET",
-      }).then(
-        function(beans) {
-            console.log(beans);
-          console.log("");
-          for (let i = 0; i < beans.length; i++) {
-              $("#list-option").append(`<div class = "coffeename" data-id = "${beans[i].id}>${beans[i].coffeehousename}</div>`)
-          }
-        }
+  $.ajax("/api/coffeehouse/" + id, {
+    type: "GET"
+  }).then(function(beans) {
+    console.log(beans);
+    console.log("");
+    for (let i = 0; i < beans.length; i++) {
+      $("#list-option").append(
+        `<div class = "coffeename" data-id = "${beans[i].id}>${beans[i].coffeeHouseName}</div>`
       );
-    });
+    }
+  });
+});
+
+$(function() {
+  $.ajax("/api/user-profile/", {
+    type: "GET"
+  }).then(function(users) {
+    console.log(users);
+    console.log("");
+    for (let i = 0; i < users.length; i++) {
+      $("#list-option").append(
+        `<div class = "username" data-id = "${users[i].id}>${users[i].userProfileName}</div>`
+      );
+    }
+  });
+});
+
+$(function() {
+  $.ajax("/api/view-user/", {
+    type: "GET"
+  }).then(function(toView) {
+    console.log(toView);
+    console.log("");
+    for (let i = 0; i < toView.length; i++) {
+      $("#list-option").append(
+        `<div class = "viewname" data-id = "${toView[i].id}>${toView[i].viewUserName}</div>`
+      );
+    }
+  });
+});
