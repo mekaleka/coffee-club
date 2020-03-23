47f0d0d8e4a88c33c0cf6711b418bdd1ce0912de
Tucker <tuckerbeauchamp@gmail.com>
2020-03-22 21:40:54 -0700

    get base functionality working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,35 @@ var db = require("../models");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
+    // if no user is signed in render the index view
     db.CoffeeHouse.findAll().then(function(data) {
-    res.render("index", data);
+      if (!req.user) {
+        return res.render("index", {
+          coffeeshops: data.map(shop => shop.toJSON())
+        });
+      }
+      res.render("coffeeshops", {
+        coffeeshops: data.map(shop => shop.toJSON())
+      });
+    });
   });
+
+  app.get("/signup", function(req, res) {
+    res.render("signup");
   });
-  app.get("/coffeehouse", function(req, res) {
-    db.CoffeeHouse.findAll().then(function(data) {
-      console.log(data);
-      res.render("coffeehouse", {
-        coffeehouse: data
+
+  app.get("/shop/:id", function(req, res) {
+    db.CoffeeHouse.findAll().then(function(dbCoffeeHouses) {
+      db.CoffeeHouse.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [db.Review]
+      }).then(function(dbCoffeHouse) {
+        res.render("coffeehouse", {
+          ...dbCoffeHouse.toJSON(),
+          coffeeshops: dbCoffeeHouses.map(shop => shop.toJSON())
+        });
       });
     });
   });
@@ -22,28 +42,7 @@ module.exports = function(app) {
         id: req.params.id
       }
     }).then(function(dbUser) {
-      const userObj = {
-        user: dbUser
-      };
-      console.log(userObj);
-      res.render("user-profile", userObj);
+      res.render("user-profile", dbUser.toJSON());
     });
-
-    res.render("user-profile");
-  });
-
-  app.get("/user/:id", function(req, res) {
-    db.Reviews.findAll({
-        where: {
-          UserId: req.params.userid
-        }
-      }).then(function(dbReviews) {
-
-       const urObject = {
-           userReviews: dbReviews
-       };
-       console.log(urObject);
-       res.render("view-user");
-      })
   });
 };
